1fa37b223f8e496abd15f66fe1a54e773fd18a7b
Will Nixon <will_nixon@me.com>
2024-12-07 13:58:18 -0500

    Initial Commit

---
+++ Nobody Panic/Nobody_PanicApp.swift
@@ -0,0 +1,20 @@
+//
+//  Nobody_PanicApp.swift
+//  Nobody Panic
+//
+//  Created by Will Nixon on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Nobody_PanicApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ Nobody PanicTests/Nobody_PanicTests.swift
@@ -0,0 +1,16 @@
+//
+//  Nobody_PanicTests.swift
+//  Nobody PanicTests
+//
+//  Created by Will Nixon on 12/7/24.
+//
+
+import Testing
+
+struct Nobody_PanicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
